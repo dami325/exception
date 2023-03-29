d4295bb92471976d89f3b39fcece3454bcf6abe6
Judalm Park (dami) <wnekfa1004@naver.com>
2023-03-29 20:08:05 +0900

    서블릿 예외 처리 - 필터

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import lombok.extern.java.Log;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST); // dispatcherType이 맞을경우 호출됨 default = REQUEST
+        return filterRegistrationBean;
+    }
+}
