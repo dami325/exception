919c7588a55d30743334be0831b01a8d8fd0adf2
Judalm Park (dami) <wnekfa1004@naver.com>
2023-03-29 19:45:22 +0900

    서블릿 예외 처리 - 오류 화면 제공

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,28 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * 오류 페이지 커스텀
+ *
+ * 설정 후 bean 등록하고
+ * 처리할 컨트롤러가 필요함
+ */
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");//오류가 발생했을때 호출할 위치
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); // RuntimeException 또는 그 자식 타입 예외
+        
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx); // 에러페이지 등록
+    }
+}
